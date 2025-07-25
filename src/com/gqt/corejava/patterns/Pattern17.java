021ff75224533b465ebc0555887436e7ef564de8
JYOTHIRMAYI <JYOTHIRMAYI@LAPTOP-AQUHVK7P.lan>
2025-07-25 09:07:29 +0530

    loaded all pattern programs

---
+++ src/com/gqt/corejava/patterns/Pattern17.java
@@ -0,0 +1,24 @@
+package com.gqt.corejava.patterns;
+
+import java.util.Scanner;
+
+public class Pattern17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+	     System.out.println("enter the count");
+	     int n=sc.nextInt();
+	     int a=1;
+	     for(int i=1;i<=n;i++) {
+	    	 for(int j=1;j<=i;j++) {
+	    		 System.out.print(a+" ");
+	    		a++; 
+	    	 }
+	    	 System.out.println();
+	     }
+
+		}
+
+	}
+
